40e8321bdd266aef31667101ef8977b4d2fb2059
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-04-18 13:17:15 +0300

    Update matrixs.hpp

--- yasakova_a_m/prj.lab/matrixs/include/matrixs/matrixs.hpp
+++ yasakova_a_m/prj.lab/matrixs/include/matrixs/matrixs.hpp
@@ -1,12 +1,14 @@
-
 #ifndef LIB_MATRIXS_HPP
 #define LIB_MATRIXS_HPP
 #include <cstddef>
 #include <tuple>
+#include <utility>
 
 class MatrixS {
 public:
     using SizeType = std::tuple<std::ptrdiff_t, std::ptrdiff_t>;
+
+public:
     ptrdiff_t rows_;
     ptrdiff_t cols_;
     int* data_;
@@ -14,25 +16,26 @@ public:
 
 public:
     MatrixS();
-    explicit MatrixS(const SizeType &size);
+    explicit MatrixS(const SizeType &size = {0, 0});
     MatrixS(const std::ptrdiff_t m, const std::ptrdiff_t n);
     ~MatrixS();
 
     MatrixS(const MatrixS &other);
-    MatrixS &operator=(const MatrixS &other);
+    MatrixS& operator=(const MatrixS& other);
 
     void resize(const SizeType &new_size);
     void resize(const std::ptrdiff_t i, const std::ptrdiff_t j);
 
-    [[nodiscard]] int &at(const SizeType &elem);
-    [[nodiscard]] const int &at(const SizeType &elem) const;
-    [[nodiscard]] int &at(const std::ptrdiff_t i, const std::ptrdiff_t j);
-    [[nodiscard]] const int &at(const std::ptrdiff_t i, const std::ptrdiff_t j) const;
+    [[nodiscard]] const int& at(const std::ptrdiff_t, const std::ptrdiff_t) const;
+    [[nodiscard]] int& at(const std::ptrdiff_t, const std::ptrdiff_t);
+    [[nodiscard]] const int& at(const SizeType) const;
+    [[nodiscard]] int& at(const SizeType);
 
-    const SizeType &ssize() const noexcept;
+    [[nodiscard]] const SizeType& ssize() const noexcept;
     [[nodiscard]] std::ptrdiff_t nRows() const noexcept;
     [[nodiscard]] std::ptrdiff_t nCols() const noexcept;
 
 };
 
 #endif //LIB_MATRIXS_HPP
+
