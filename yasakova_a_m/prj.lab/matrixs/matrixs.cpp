81ee82f1794df8cee6ca5d341cf670d7804927f6
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-04-28 18:31:51 +0300

    Update matrixs.cpp

--- yasakova_a_m/prj.lab/matrixs/matrixs.cpp
+++ yasakova_a_m/prj.lab/matrixs/matrixs.cpp
@@ -1,93 +1,123 @@
 #include <matrixs/matrixs.hpp>
 #include <iostream>
-#include <cmath>
-#include <vector>
 
-MatrixS::MatrixS(){
+MatrixS::MatrixS()
+{
     rows_ = 0;
+    len_ = 0;
     cols_ = 0;
     data_ = nullptr;
-    size_ = 0;
 }
-MatrixS::MatrixS(const SizeType& size) : rows_{ std::get<0>(size) }, cols_{ std::get<1>(size) }, data_(nullptr) {
+
+MatrixS::MatrixS(const SizeType& s) : rows_(std::get<0>(s)), cols_(std::get<1>(s)), data_(nullptr)
+{
     if (rows_ <= 0 || cols_ <= 0) {
         throw std::invalid_argument("Sizes must be positive");
     }
-    size_ = rows_ + rows_ * cols_;
-    data_ = new int[size_];
+    len_ = rows_ + rows_ * cols_;
+    data_ = new int[len_];
     for (std::ptrdiff_t i = 0; i < rows_; ++i) {
         data_[i] = rows_ + i * cols_;
     }
-    for (std::ptrdiff_t i = rows_; i < size_; ++i) {
+    for (std::ptrdiff_t i = rows_; i < len_; ++i) {
         data_[i] = 0;
     }
 }
 
-MatrixS::MatrixS(const std::ptrdiff_t m, const std::ptrdiff_t n) : rows_{ m }, cols_{ n }, data_(nullptr) {
+MatrixS::MatrixS(std::ptrdiff_t rowsInp_, std::ptrdiff_t colsInp_) : rows_(rowsInp_), cols_(colsInp_), data_(nullptr){
     if (rows_ <= 0 || cols_ <= 0) {
         throw std::invalid_argument("Sizes must be positive");
     }
-    data_ = new int[size_];
+    len_ = rows_ + rows_ * cols_;
+    data_ = new int[len_];
     for (std::ptrdiff_t i = 0; i < rows_; ++i) {
         data_[i] = rows_ + i * cols_;
     }
-    for (std::ptrdiff_t i = rows_; i < size_; ++i) {
+    for (std::ptrdiff_t i = rows_; i < len_; ++i) {
         data_[i] = 0;
     }
 }
 
-MatrixS::MatrixS(const MatrixS& other) : rows_{ other.rows_ }, cols_{ other.cols_ }, size_{other.size_}, data_(nullptr) {
-    if (this == &other) {
+MatrixS::MatrixS(std::ptrdiff_t rowsInp_, std::ptrdiff_t colsInp_, int num) : rows_(rowsInp_), cols_(colsInp_), len_(rowsInp_ + rowsInp_ * colsInp_), data_(nullptr){
+    if (rows_ <= 0 || cols_ <= 0) {
+        throw std::invalid_argument("Sizes must be positive");
+    }
+    //len_ = rows_ + rows_ * cols_;
+    data_ = new int[len_];
+    for (std::ptrdiff_t i = 0; i < rows_; ++i) {
+        data_[i] = rows_ + i * cols_;
+    }
+    for (std::ptrdiff_t i = rows_; i < len_; ++i) {
+        data_[i] = num;
+    }
+}
+
+MatrixS::MatrixS(std::initializer_list<std::initializer_list<int>> initList)
+{
+    std::ptrdiff_t rowsNew_ = initList.size();
+    if (rowsNew_ == 0) {
+        throw std::invalid_argument("Number of rows cannot be 0");
+    }
+    rows_ = rowsNew_;
+    std::ptrdiff_t colsNew_ = initList.begin()->size();
+    if (colsNew_ == 0) {
+        throw std::invalid_argument("Number of columns cannot be 0");
+    }
+    cols_ = colsNew_;
+    len_ = rows_ + rows_ * cols_;
+    data_ = new int[len_];
+    std::ptrdiff_t i = 0;
+    for (std::ptrdiff_t i = 0; i < rows_; ++i) {
+        data_[i] = rows_ + i * cols_;
+    }
+    for (auto subList: initList) {
+        if (colsNew_ != subList.size()) {
+            throw std::invalid_argument("Rows must have the same size");
+        }
+        std::copy(subList.begin(), subList.end(), data_ + rows_ + cols_ * i);
+        ++i;
+    }
+}
+
+MatrixS::MatrixS(const MatrixS& prev) : rows_(prev.rows_), cols_(prev.cols_), len_(prev.len_), data_(nullptr){
+    if (this == &prev) {
         return;
     }
-    data_ = new int[size_];
-    std::copy(other.data_, other.data_ + rows_ * cols_, data_);
+    data_ = new int[len_];
+    std::copy(prev.data_, prev.data_ + len_, data_);
 }
 
-MatrixS::~MatrixS() {
+//MatrixS::MatrixS(MatrixS&& prev) noexcept : rows_(prev.rows_), cols_(prev.cols_), len_(prev.len_), data_(nullptr){
+//    if (this == &prev) {
+//        return;
+//    }
+//    data_ = new int[len_];
+//    std::copy(prev.data_, prev.data_ + len_, data_);
+//} 
+
+MatrixS::~MatrixS()
+{
     delete[] data_;
 }
-
-MatrixS& MatrixS::operator=(const MatrixS& other)
-    {
-        if (this == &other) {
-            return *this;
-        }
-        rows_ = other.rows_;
-        cols_ = other.cols_;
-        size_ = other.size_;
-        if (data_ != nullptr) {
-            delete[] data_;
-        }
-        data_ = new int[size_];
-        for (std::ptrdiff_t i = 0; i < size_; ++i) {
-            data_[i] = other.data_[i];
-        }
-        return *this;
-    }
-
 
 const int& MatrixS::at(const std::ptrdiff_t row, const std::ptrdiff_t col) const {
     if (row >= rows_ || col >= cols_ || row < 0 || col < 0) {
         throw std::out_of_range("Wrong position");
     }
-    return data_[data_[row] + col]; //это указатель на начало строки матрицы с индексом row
+    return data_[data_[row] + col];
 }
-
 int& MatrixS::at(const std::ptrdiff_t row, const std::ptrdiff_t col) {
     if (row >= rows_ || col >= cols_ || row < 0 || col < 0) {
         throw std::out_of_range("Wrong position");
     }
     return data_[data_[row] + col];
 }
-
 const int& MatrixS::at(const MatrixS::SizeType s) const{
     if (std::get<0>(s) >= rows_ || std::get<1>(s) >= cols_ || std::get<0>(s) < 0 || std::get<1>(s) < 0) {
         throw std::out_of_range("Wrong position");
     }
     return data_[data_[std::get<0>(s)] + std::get<1>(s)];
 }
-
 int& MatrixS::at(const MatrixS::SizeType s) {
     if (std::get<0>(s) >= rows_ || std::get<1>(s) >= cols_ || std::get<0>(s) < 0 || std::get<1>(s) < 0) {
         throw std::out_of_range("Wrong position");
@@ -95,11 +125,11 @@ int& MatrixS::at(const MatrixS::SizeType s) {
     return data_[data_[std::get<0>(s)] + std::get<1>(s)];
 }
 
-[[nodiscard]] std::ptrdiff_t MatrixS::nRows() const noexcept
+std::ptrdiff_t MatrixS::nRows() const noexcept
 {
     return rows_;
 }
-[[nodiscard]] std::ptrdiff_t MatrixS::nCols() const noexcept
+std::ptrdiff_t MatrixS::nCols() const noexcept
 {
     return cols_;
 }
@@ -141,7 +171,8 @@ void MatrixS::resize(const SizeType& s)
     *this = newMatrix;
 }
 
-const MatrixS::SizeType &MatrixS::ssize() const noexcept{
+MatrixS::SizeType MatrixS::ssize() const noexcept
+{
     return SizeType(rows_, cols_);
 }
 
